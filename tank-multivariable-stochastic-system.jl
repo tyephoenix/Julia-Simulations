c8961677d996247ab4d6b555a47cef45d84c1f73
Tye Phoenix <tphoenix@mit.edu>
2024-06-03 16:29:07 -0400

    Update tank-multivariable-stochastic-system.jl
    Added stochastic version.

--- tank-multivariable-stochastic-system.jl
+++ tank-multivariable-stochastic-system.jl
@@ -6,7 +6,7 @@ using Plots; gr()
 # Vars
 tSpan = (0.0,15.0)
 a = (1,1)
-b = 0.4
+b = 0
 v1_0 = 5
 v2_0 = 10
 
@@ -17,11 +17,18 @@ f2(t) = 0
 
 function func(du, u, p, t) 
     du[1] = f1(t) - a[1]*u[1] + b*a[2]*u[2] 
-    du[2] = f2(t) - a[2]*u[2] - b*a[2]*u[2]
+    du[2] = f2(t) + a[1]*u[1] - a[2]*u[2] - b*a[2]*u[2]
     du[3] = (1-b)*a[2]*u[2]
 end
 
-problem = ODEProblem(func, [v1_0; v2_0; 0], tSpan)
+function noise(du, u, p, t) 
+    du[1] = 0.3
+    du[2] = 0.3
+    du[3] = 0
+end
+
+dt = 1/(2^(4))
+problem = SDEProblem(func, noise, [v1_0; v2_0; 0], tSpan)
 
 
 # Plot
@@ -32,7 +39,7 @@ function PlotInputs()
 end
 
 function PlotVolume()
-    sol = solve(problem, Tsit5())
+    sol = solve(problem, EM(), dt=dt)
 
     plot!(p, sol, label=["Vol. Tank 1" "Vol. Tank 2" "Wash"])
 end
